b208884668f6ac2918940a8af4f0fc148fc95002
jbucsa <justin.bucsa@gmail.com>
2025-08-27 18:43:50 -0500

    Begun adding Redis cache functionality to AppHost program.

--- Weather_Hub_output/start/AppHost/AppHost.cs
+++ Weather_Hub_output/start/AppHost/AppHost.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cache = builder.AddRedis("cache");
+
 var api = builder.AddProject<Projects.Api>("api");
 
 var web = builder.AddProject<Projects.MyWeatherHub>("myweatherhub")
